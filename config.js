7f4b94b41a024c1656e81b0109f70019a9a6fa11
CodingMaster27 <236492306+CodingMaster27@users.noreply.github.com>
2026-06-19 22:24:16 -0500

    Update view password

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Password your dad uses to view the site
-const VIEW_PASSWORD = 'iloveyoudad';
+const VIEW_PASSWORD = '032712';
 
 // Password you use to add/edit/delete cards (keep this to yourself)
 const ADMIN_PASSWORD = 'admin1234';
