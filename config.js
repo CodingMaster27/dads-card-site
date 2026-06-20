70e894bf40c39dbcdf955d477197d2188ff4cc8f
CodingMaster27 <236492306+CodingMaster27@users.noreply.github.com>
2026-06-19 22:25:10 -0500

    Set admin password; restore placeholder view password

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Password your dad uses to view the site
-const VIEW_PASSWORD = '032712';
+const VIEW_PASSWORD = 'iloveyoudad';
 
 // Password you use to add/edit/delete cards (keep this to yourself)
-const ADMIN_PASSWORD = 'admin1234';
+const ADMIN_PASSWORD = '032712';
